a117a71d83970ed1f9ffa92668d52c54bec02239
rubans <nkusrq@163.com>
2021-03-01 17:47:24 +0800

    3.1 all

--- mock/tx/isPalindrome.go
+++ mock/tx/isPalindrome.go
@@ -1,30 +1,32 @@
 package main
 
+import "strings"
+
 func isPalindrome(s string) bool {
-    if len(s) == 0 {
-        return true
-    }
-    s = strings.ToLower(s)
-    i, j := 0, len(s) - 1
-    for i < j {
-        for !valid(s[i]) && i < j {
-            i++
-        }
-        for !valid(s[j]) && i < j {
-            j--
-        }
-        if i < j {
-            if s[i] != s[j] {
-                // fmt.Println("s[i]", s[i], "s[j]", s[j])
-                return false
-            }
-            i++
-            j--
-        }
-    }
-    return true
+	if len(s) == 0 {
+		return true
+	}
+	s = strings.ToLower(s)
+	i, j := 0, len(s)-1
+	for i < j {
+		for !valid(s[i]) && i < j {
+			i++
+		}
+		for !valid(s[j]) && i < j {
+			j--
+		}
+		if i < j {
+			if s[i] != s[j] {
+				// fmt.Println("s[i]", s[i], "s[j]", s[j])
+				return false
+			}
+			i++
+			j--
+		}
+	}
+	return true
 }
 
 func valid(b byte) bool {
-    return (b >= 'a' && b <= 'z') || (b >= 'A' && b <= 'Z') || (b >= '0' && b <= '9')
+	return (b >= 'a' && b <= 'z') || (b >= 'A' && b <= 'Z') || (b >= '0' && b <= '9')
 }
