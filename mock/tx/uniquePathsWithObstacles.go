959b40ef378a424702df71c15063f70b177c1d24
rubans <nkusrq@163.com>
2021-03-12 19:06:24 +0800

    mar week1 all

--- mock/tx/uniquePathsWithObstacles.go
+++ mock/tx/uniquePathsWithObstacles.go
@@ -1,6 +1,38 @@
 package main
 
 func uniquePathsWithObstacles(obstacleGrid [][]int) int {
+	dp := make([][]int, len(obstacleGrid))
+	for i := range dp {
+		dp[i] = make([]int, len(obstacleGrid[0]))
+	}
+	dp[0][0] = 1
+
+	for i := 0; i < len(obstacleGrid); i++ {
+		for j := 0; j < len(obstacleGrid[0]); j++ {
+			if obstacleGrid[i][j] == 1 {
+				dp[i][j] = 0
+				continue
+			}
+			if x, y := i, j-1; y >= 0 {
+				dp[i][j] += dp[x][y]
+			}
+			if x, y := i-1, j; x >= 0 {
+				dp[i][j] += dp[x][y]
+			}
+		}
+	}
+	// for i := 0; i < len(dp); i++ {
+	//     for j := 0; j < len(dp[0]); j++ {
+	//         fmt.Println(dp[i][j], " ")
+	//     }
+	//     fmt.Println()
+	// }
+
+	return dp[len(dp)-1][len(dp[0])-1]
+}
+
+/* TLE
+func uniquePathsWithObstacles(obstacleGrid [][]int) int {
 	if obstacleGrid[0][0] == 1 || obstacleGrid[len(obstacleGrid)-1][len(obstacleGrid[0])-1] == 1 {
 		return 0
 	}
@@ -25,3 +57,4 @@ func walk(grid [][]int, x, y int) int {
 
 	return ret
 }
+*/
